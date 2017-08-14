70ea3f640e4b88271c5c30823829b3f4de4613d7
b3n10 <francisben.lleve@gmail.com>
2017-08-14 16:11:55 +0800

    Automated game with random numbers

--- guessing-game.js
+++ guessing-game.js
@@ -1,12 +1,32 @@
+/*
+ * Author: Francis Ben Lleve
+ * Vanila JS
+ * Randomize numbers compared against each other
+ * Most rounds of win, wins the game
+ *
+*/
+
 function Player(name) {
   this.name = name;
   this.number = 0;
-}
+  this.win_count = 0;
+};
 
 Player.prototype.randomme = function() {
   min = Math.ceil(1);
   max = Math.floor(100);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+Player.prototype.getrandom = function() {
+  while (array_number.length < 100) {
+    var i = this.randomme();
+    if (array_number.indexOf(i) < 0) {
+      array_number.push(i);
+      return i;
+    }
+  }
+  return false;
 };
 
 var p1 = new Player('ben');
@@ -18,11 +38,20 @@ var x, y;
 console.log(p1.name + " vs " + p2.name);
 
 var fighting = setInterval(function() {
-  x = makerandom(p1);
-  y = makerandom(p2);
+  x = p1.getrandom();
+  y = p2.getrandom();
 
   if (x === false && y === false) {
     clearInterval(fighting);
+
+    if (p1.win_count > p2.win_count) {
+      console.log(p1.name + " with " + p1.win_count + " wins is the Overall Champion!");
+    } else if (p1.win_count < p2.win_count) {
+      console.log(p2.name + " with " + p2.win_count + " wins is the Overall Champion!");
+    } else {
+      console.log("It's a Draw!");
+    }
+    
     return;
   }
   
@@ -32,24 +61,12 @@ var fighting = setInterval(function() {
 
   if (x > y) {
     console.log(p1.name + " wins !\n");
+    p1.win_count++;
   } else if (x < y) {
     console.log(p2.name + " wins !\n");
-  } else if (x === y) {
-    console.log("It's a draw!");
-    clearInterval(fighting);
+    p2.win_count++;
   }
 
   round_number++;
 }, 1000);
-
-function makerandom(obj) {
-  while (array_number.length < 100) {
-    var i = obj.randomme();
-    if (array_number.indexOf(i) < 0) {
-      array_number.push(i);
-      return i;
-    }
-  }
-  return false;
-}
 
